63922e5a7adf817bc275cfbbafd21bd88ba08d1f
Bansah <dennisbansah383@gmail.com>
2025-11-28 07:27:48 -0800

    Update local.properties for SDK paths, add flutter_lldb_helper.py and flutter_lldbinit files, and enhance MonoConnectWebDialog for web compatibility

--- budgetPal-main/lib/widgets/mono_connect_web.dart
+++ budgetPal-main/lib/widgets/mono_connect_web.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:ui' as ui;
 import 'dart:html' as html;
 import 'dart:convert';
+import 'dart:js_interop';
+
+@JS()
+external get platformViewRegistry;
 
 typedef MonoCodeCallback = void Function(String code);
 
 class MonoConnectWebDialog extends StatefulWidget {
   final String url;
   final MonoCodeCallback onCode;
-  const MonoConnectWebDialog({super.key, required this.url, required this.onCode});
+  const MonoConnectWebDialog({
+    super.key,
+    required this.url,
+    required this.onCode,
+  });
 
   @override
   State<MonoConnectWebDialog> createState() => _MonoConnectWebDialogState();
@@ -31,7 +38,9 @@ class _MonoConnectWebDialogState extends State<MonoConnectWebDialog> {
             data = jsonDecode(data);
           } catch (_) {}
         }
-        if (data is Map && data['type'] == 'mono.code' && data['code'] != null) {
+        if (data is Map &&
+            data['type'] == 'mono.code' &&
+            data['code'] != null) {
           widget.onCode(data['code']);
           Navigator.of(context).pop();
         }
@@ -50,17 +59,22 @@ class _MonoConnectWebDialogState extends State<MonoConnectWebDialog> {
 
   @override
   Widget build(BuildContext context) {
-    // Register a view type for the iframe
-    // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory(
-      'mono-connect-iframe',
-      (int viewId) => html.IFrameElement()
-        ..src = widget.url
-        ..style.border = 'none'
-        ..width = '100%'
-        ..height = '100%'
-        ..allow = 'clipboard-read; clipboard-write',
-    );
+    // Register a view type for the iframe (web only)
+    if (kIsWeb) {
+      platformViewRegistry.callMethod('registerViewFactory', [
+        'mono-connect-iframe',
+        (int viewId) {
+          final element =
+              html.IFrameElement()
+                ..src = widget.url
+                ..style.border = 'none'
+                ..width = '100%'
+                ..height = '100%'
+                ..allow = 'clipboard-read; clipboard-write';
+          return element;
+        }.toJS,
+      ]);
+    }
     return Dialog(
       insetPadding: const EdgeInsets.all(16),
       child: SizedBox(
@@ -68,9 +82,7 @@ class _MonoConnectWebDialogState extends State<MonoConnectWebDialog> {
         height: 600,
         child: Stack(
           children: [
-            const Positioned.fill(
-              child: ColoredBox(color: Colors.white),
-            ),
+            const Positioned.fill(child: ColoredBox(color: Colors.white)),
             HtmlElementView(viewType: 'mono-connect-iframe'),
             Positioned(
               top: 8,
